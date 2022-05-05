e8f90fc1f2af26439441da85b5d9ea50bcddea9d
poyaowang <michael90110@gmail.com>
2022-05-05 15:25:50 +0900

    Combine form into timeEntryForm

--- src/components/timeEntryForm.jsx
+++ src/components/timeEntryForm.jsx
@@ -2,10 +2,11 @@ import Joi from "joi-browser";
 import queryString from "query-string";
 import React, { Component } from "react";
 
-import Form from "./common/form";
 import TimeEntryServices from "../services/TimeEntryServices";
 
 import config from "../config.json";
+import Select from "./common/Select";
+import Input from "./common/Input";
 
 const apiKey = config.apiKey;
 const workspaceId = config.workspaceId;
@@ -13,7 +14,7 @@ const userId = config.userId;
 
 const service = new TimeEntryServices(apiKey, workspaceId, userId);
 
-class TimeEntryForm extends Form {
+class TimeEntryForm extends Component {
   state = {
     data: {
       projectId: "",
@@ -47,6 +48,83 @@ class TimeEntryForm extends Form {
     this.setState({
       data: { projectId, taskId, tagId, description, hours, minutes },
     });
+  }
+
+  validate = () => {
+    const options = { abortEarly: false };
+    const { error } = Joi.validate(this.state.data, this.schema, options);
+    if (!error) return null;
+
+    const errors = {};
+    for (let item of error.details) errors[item.path[0]] = item.message;
+    return errors;
+  };
+
+  validateProperty = ({ name, value }) => {
+    const obj = { [name]: value };
+    const schema = { [name]: this.schema[name] };
+    const { error } = Joi.validate(obj, schema);
+    return error ? error.details[0].message : null;
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    const errors = this.validate();
+    this.setState({ errors: errors || {} });
+    if (errors) return;
+
+    this.doSubmit();
+  };
+
+  handleChange = ({ currentTarget: input }) => {
+    const errors = { ...this.state.errors };
+    const errorMessage = this.validateProperty(input);
+    if (errorMessage) errors[input.name] = errorMessage;
+    else delete errors[input.name];
+
+    const data = { ...this.state.data };
+    data[input.name] = input.value;
+
+    this.setState({ data, errors });
+  };
+
+  renderButton(label) {
+    return (
+      <button disabled={this.validate()} className="btn btn-primary">
+        {label}
+      </button>
+    );
+  }
+
+  renderSelect(name, label, options) {
+    const { data, errors } = this.state;
+
+    return (
+      <Select
+        name={name}
+        value={data[name]}
+        label={label}
+        options={options}
+        onChange={this.handleChange}
+        error={errors[name]}
+      />
+    );
+  }
+
+  renderInput(name, label, type = "text") {
+    const { data, errors } = this.state;
+
+    return (
+      <Input
+        type={type}
+        name={name}
+        value={data[name]}
+        label={label}
+        onChange={this.handleChange}
+        error={errors[name]}
+      />
+    );
   }
 
   doSubmit = async () => {
